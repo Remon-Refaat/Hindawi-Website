76ca69bd62da9123b3d7c17a6256c66cc7fb6a4e
Doaa Asl <doaa.asl@hindawi.local>
2018-07-12 15:25:32 +0200

    Last Version in Search Manuscript

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,27 +1,27 @@
 require 'selenium-webdriver'
+require 'rspec'
+include ::RSpec::Matchers
+require 'rspec/expectations'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/cucumber'
-# require 'capybara/rspec'
-require 'rspec'
-require 'faker'
 require 'capybara-screenshot/cucumber'
-require 'wannabe_bool'
-require 'rspec/expectations'
+require 'tiny_tds'
+require 'pdf-reader'
+PDF::Reader::MalformedPDFError
 
-#############################################
-################ Capybara ###################
-#############################################
-# path = Dir.pwd + '/resources/chromedriver.exe'
-# Selenium::WebDriver::Chrome.driver_path = path
-# Capybara.default_driver = :selenium_chrome
+ENV['PROJECT_DIR'] = Dir.pwd
+ENV['DATA_DIR'] = ENV['PROJECT_DIR'] + '/data'
+ENV['SCREENSHOTS_DIR'] = ENV['PROJECT_DIR'] + '/screenshots'
+
+
 Capybara.register_driver :chrome do |app|
   path = Dir.pwd
   chrome_path  = path + '/resources/chromedriver.exe'
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--start-maximized" ]})
   Selenium::WebDriver::Chrome::driver_path = chrome_path
   pref = {
-      download: {
+      downloads: {
           prompt_for_download: false,
           default_directory: ENV['DOWNLOAD_DIR']
       }
@@ -30,25 +30,10 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara::default_driver = :chrome
-
-
-
-#############################################
-########### Environment Variables ###########
-#############################################
-ENV['PROJECT_DIR'] = Dir.pwd
-ENV['DATAPATH'] = ENV['PROJECT_DIR'] + '/data/'
-ENV['SCREENSHOTS_DIR'] = ENV['PROJECT_DIR'] + '/screenshots'
-ENV['Download_Dir'] = ENV['PROJECT_DIR'] + '/downloads'
-
-#############################################
-########### Configuration ###################
-#############################################
 Capybara.default_selector = :xpath
-Capybara.default_max_wait_time = 0.5
+Capybara.default_max_wait_time = 2
 Capybara.save_path = ENV['SCREENSHOTS_DIR']
-
 Capybara::Screenshot.autosave_on_failure = false
 Capybara::Screenshot.append_timestamp = false
-
-
+Capybara::Node::Element
+ENV['DOWNLOAD_DIR'] = Dir.pwd + '/downloads'
